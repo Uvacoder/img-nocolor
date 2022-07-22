eea2904f56c501596e5c3d020fb36493655c040f
Lukáš Sitter <lukas.sitter@gmail.com>
2022-07-22 18:25:28 -0400

    Fix header dark mode

--- components/header/header.tsx
+++ components/header/header.tsx
@@ -3,7 +3,7 @@ import { FC } from 'react';
 import Switch from '../switch/switch';
 
 const Header:FC = () => (
-  <div className='flex sticky justify-around items-center z-20 bg-white/70 dark:bg-black/70 dark:bg-white top-0 h-20'>
+  <div className='flex sticky justify-around items-center z-20 bg-white/70 dark:bg-black/70 top-0 h-20'>
     <div className='tracking-wider cursor-pointer '>B&W</div>
     <Switch />
   </div>
