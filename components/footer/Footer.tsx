ffc6d419b5a40c521e6fbc703fd9d8a0409dc2ed
Lukáš Sitter <lukas.sitter@gmail.com>
2022-07-22 19:27:50 -0400

    login modal on hover

--- components/footer/Footer.tsx
+++ components/footer/Footer.tsx
@@ -1,14 +1,24 @@
 import { FC } from 'react';
+import Link from 'next/link';
 
 import { footerLinks } from './footerData';
 
 const Footer: FC = () => (
   <footer>
-    <div className='flex fixed h-20 w-screen justify-around items-center bottom-0 bg-white/70 dark:bg-black/70'>
-      <div className='flex gap-4 sm:gap-14 dark:text-white'>
-        {footerLinks.map((item) => <div>{item.link}</div>)}
+    <section className='group flex fixed h-20 w-screen justify-around items-center bottom-0 bg-white/70 dark:bg-black/70'>
+      <div className='relative flex h-fit gap-4 sm:gap-14 dark:text-white'>
+        {footerLinks.map((item) => (
+          <>
+            <Link href='/#' className='normal'>
+              {item.link}
+            </Link>
+            <div className='hidden group-hover:flex absolute flex justify-center right-1/2 translate-x-1/2 bottom-1/2 -translate-y-1/2 w-screen h-20 items-center bg-white dark:bg-black cursor-pointer'>
+              <div className='hover:scale-105 hover: ease-in-out hover:transition'>Login to use</div>
+            </div>
+          </>
+        ))}
       </div>
-    </div>
+    </section>
   </footer>
 );
 
