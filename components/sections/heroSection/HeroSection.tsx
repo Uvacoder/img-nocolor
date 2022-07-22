a9de1862db07c5768f43f9d5da1f1756f111b73a
Lukáš Sitter <lukas.sitter@gmail.com>
2022-07-22 18:42:53 -0400

    responsivity on big screens

--- components/sections/heroSection/HeroSection.tsx
+++ components/sections/heroSection/HeroSection.tsx
@@ -5,7 +5,11 @@ import { heroSectionData } from './heroSection.data';
 
 const HeroSection: FC = () => (
   <div className='flex flex-3 flex-wrap mx-8 opacity-80 align-center justify-center dark:bg-black gap-8'>
-    {heroSectionData.map((item) => <Image height={300} width={300} src={item.url} />)}
+    {heroSectionData.map((item) => (
+      <div className='relative w-[19rem] h-[19rem] 2xl:w-[25rem] 2xl:h-[25rem]'>
+        <Image src={item.url} layout='fill' objectFit='cover' />
+      </div>
+    ))}
   </div>
 );
 
