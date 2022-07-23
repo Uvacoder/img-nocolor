d894fe5d093348241558e1b972ace40d88735264
Lukáš Sitter <lukas.sitter@gmail.com>
2022-07-23 11:26:50 -0400

    Change responsivity

--- components/feed/Feed.tsx
+++ components/feed/Feed.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image';
 import { feedTypes } from './feedTypes';
 
 const Feed: FC<feedTypes> = ({ item }) => (
-  <div className='relative w-36 h-36 sm:w-44 sm:h-44 md:w-64 md:h-64 lg:w-[20rem] lg:h-[20rem] 2xl:w-[25rem] 2xl:h-[25rem] grayscale'>
+  <div className='relative w-24 h-24 sm:w-44 sm:h-44 md:w-64 md:h-64 lg:w-[20rem] lg:h-[20rem] 2xl:w-[25rem] 2xl:h-[25rem] grayscale'>
     <Image src={item.url} layout='fill' objectFit='cover' />
   </div>
 );
