f3cbc965ca0bb1a77a8c466e3cc37cd13f0ab6c1
Lukáš Sitter <lukas.sitter@gmail.com>
2022-07-22 22:03:46 -0400

    Handle change to black and white

--- components/sections/heroSection/HeroSection.tsx
+++ components/sections/heroSection/HeroSection.tsx
@@ -6,7 +6,7 @@ import { heroSectionData } from './heroSection.data';
 const HeroSection: FC = () => (
   <div className='flex flex-3 flex-wrap opacity-80 align-center justify-center dark:bg-black gap-8'>
     {heroSectionData.map((item) => (
-      <div className='relative w-36 h-36 sm:w-44 sm:h-44 md:w-60 md:h-60 lg:w-[19rem] lg:h-[19rem] 2xl:w-[25rem] 2xl:h-[25rem]'>
+      <div className='relative w-36 h-36 sm:w-44 sm:h-44 md:w-60 md:h-60 lg:w-[19rem] lg:h-[19rem] 2xl:w-[25rem] 2xl:h-[25rem] grayscale'>
         <Image src={item.url} layout='fill' objectFit='cover' />
       </div>
     ))}
