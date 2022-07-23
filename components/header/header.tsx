fe1d00086364d9c218a5e2b31639f22604af2767
Lukáš Sitter <lukas.sitter@gmail.com>
2022-07-22 20:05:04 -0400

    Change name

--- components/header/header.tsx
+++ components/header/header.tsx
@@ -4,7 +4,7 @@ import Switch from '../switch/switch';
 
 const Header:FC = () => (
   <div className='flex sticky justify-around items-center z-20 bg-white/70 dark:bg-black/70 top-0 h-20'>
-    <div className='tracking-wider cursor-pointer '>B&W</div>
+    <div className='tracking-wider cursor-pointer '>no color</div>
     <Switch />
   </div>
 );
