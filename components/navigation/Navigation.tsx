21bf389dab6e7cb25a6c45aff6b510b7b46a72f0
Lukáš Sitter <lukas.sitter@gmail.com>
2022-07-23 20:39:58 -0400

    Fix header link

--- components/navigation/Navigation.tsx
+++ components/navigation/Navigation.tsx
@@ -1,10 +1,11 @@
 import { FC } from 'react';
+import Link from 'next/Link';
 
 import Switch from '../switch/switch';
 
 const Navigation:FC = () => (
   <div className='flex sticky px-8 md:px-16 lg:px-36 justify-between items-center z-20 bg-white/70 dark:bg-black/70 top-0 h-20'>
-    <a href='/#' className='tracking-wider cursor-pointer '>no color</a>
+    <Link href='/#' className='tracking-wider cursor-pointer '>no color</Link>
     <Switch />
   </div>
 );
