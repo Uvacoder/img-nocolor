10be9b2b3b61aa1e21cc1b4850a15d67bb667289
Lukáš Sitter <lukas.sitter@gmail.com>
2022-07-24 23:26:44 -0400

    change colors for better readability

--- components/menu/BottomMenu.tsx
+++ components/menu/BottomMenu.tsx
@@ -14,7 +14,7 @@ const BottomMenu: FC<bottomMenuTypes> = ({ user, handleClick }) => {
   return (
     <footer>
       {!user ? (
-        <section className='group flex fixed h-20 w-screen justify-around items-center bottom-0 bg-white/70 dark:bg-black/70'>
+        <section className='group flex fixed h-20 w-screen justify-around items-center bottom-0 bg-white/90 dark:bg-black/90'>
           <div className='relative flex h-fit gap-6 xs:gap-12 sm:gap-16 dark:text-white'>
             {bottomMenuLinks.map((item) => (
               <div key={item.id}>
@@ -37,8 +37,8 @@ const BottomMenu: FC<bottomMenuTypes> = ({ user, handleClick }) => {
           </div>
         </section>
       ) : (
-        <section className='flex fixed h-20 w-screen justify-around bottom-0 bg-white/70 dark:bg-black/70'>
-          <div className='flex items-center gap-6  xs:gap-12 sm:gap-16 dark:text-white'>
+        <section className='flex fixed h-20 w-screen justify-around bottom-0 bg-white/90 dark:bg-black/90'>
+          <div className='flex items-center gap-6 xs:gap-12 sm:gap-16 dark:text-white'>
             {bottomMenuLinks.map((item) => (
               <div key={item.id}>
                 <div
@@ -58,7 +58,7 @@ const BottomMenu: FC<bottomMenuTypes> = ({ user, handleClick }) => {
                       </a>
                     </Link>
                   )}
-                  <div className={` ${item.href === '/account' && 'group-hover:flex'} hidden absolute h-20 justify-center items-center right-1/2 translate-x-1/2 bottom-1/2 -translate-y-1/2 pr-52 md:pr-0 w-[100rem] lg:w-[1000rem] bg-white/90 dark:bg-black/90 cursor-pointer gap-8`}>
+                  <div className={` ${item.href === '/account' && 'group-hover:flex'} hidden absolute h-20 justify-center items-center right-1/2 translate-x-1/2 bottom-1/2 -translate-y-1/2 pr-52 md:pr-0 w-[100rem] lg:w-[1000rem] bg-white/95 dark:bg-black/95 cursor-pointer gap-8`}>
                     {loginLinks.map((item) => (
                       <Link
                         key={item.id}
